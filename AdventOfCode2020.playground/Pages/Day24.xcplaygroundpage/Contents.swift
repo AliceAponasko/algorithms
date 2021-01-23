17993ce0fca0b0c87786944d3bb8afe151f20330
Alice Aponasko <alice.aponasko@gmail.com>
2021-01-23 15:05:24 -0700

    Advent of code - 24

--- AdventOfCode2020.playground/Pages/Day24.xcplaygroundpage/Contents.swift
+++ AdventOfCode2020.playground/Pages/Day24.xcplaygroundpage/Contents.swift
@@ -1,3 +1,468 @@
 import Foundation
 
+let input =
+"""
+neneneneneneeswneenene
+wwsewnewwwswnwwswwwewswweswse
+nwneseneneswnenwnenesene
+wnwnwswnwwwwwwwwsenwenwnewnwe
+neswseseswnwneweswwnwsweseswenwse
+eeneenewseneweeeseeeeeenew
+neewseeeneswsenewseewswwne
+wwwwnwwnwnwnwnwsenwwne
+nweeseswesweseeseeesenwseeenwww
+eneswwweswwswswe
+nenenwneneneneswnenenenwnene
+eswwswswnewsenenwseseseseseeswseswsese
+nwswswswseswswneswsweswswswnenenwsewsese
+eneeeeesewewenweewe
+nwnenwnwnwnenwnwwswnwnenwsenenwswenwnw
+sesesenwswswswswseswswswe
+weeneseeneeeenwswweeeesweee
+sewwwwwwwwnwnwnwenwswnwnenwwnwe
+wwwenewwwnwwwswnwwsw
+swswwswswswsweswswsesw
+nenenwsenwenenwnwnwnww
+swswnesenewswnesesesesesewswsesesesesw
+swseswswswnwseseswswwswseswneswneswswswew
+eswnwweswsewwnw
+seeeeweseeesenesesweeewnesese
+seseneseweseseseseseswwnwseswswsesenese
+nwnwswnwnwnenenwnewneenwnenwnene
+ewswswwnwnwneseswnenenwnwwesesweswee
+wwwwwseeswnwnwwsenwseeswnwnewsw
+nwwnwwsesewwnwwnwwswwnwnwneenww
+sewnewneneswneneeswneeeneneneenwnee
+eswneeeeweneneneeneeeweseee
+weeewsewnwwneeswwnwnwwsewew
+swnwweswsenwnwnwnwnwnwnwnwnwnwnenwnwe
+nwneswseswseseeeseswsesesesenenesewew
+swneswnesesewswswwswswswse
+swseewsewnwsesenesenenwesewene
+neneswseneneenenwneseneneswneewnwneswne
+swswsewseswneseswswswsesesesewseneswne
+nwswnenwneneseneenwneswnenenesenwnwnww
+eenwsweseseeneseenweeswsenweeswse
+wwswnwnwwwnwnwnwe
+wweneenenwseenenenesenenenewnwnew
+seeeneenwswseeweneseeseenwwew
+nwewnwweweenwwwwwnwnwwswnw
+nenwnenesenwnwnenwsenwnewneswsenenenwnenw
+nwneeeeneneneneeneswnenee
+swnwnwseseswswseseseswswseswsesw
+wswswswwseneenesesesesw
+eneswnenesenwswsenenwnwnenenenwnenwne
+eeseseseseseenwsesewsesenesenwesee
+swswswwwwswsewneswwsw
+swnenwseneseswseneswseenwswswesewnesesww
+neneneswswwenenenene
+wswwwwsewnwwwnewwwsewewnwsee
+wnwwsewneenwwww
+swswneswwneswswswswseseewswsw
+neswewneeswswnee
+senenwseeeesweseeeswseeeenwee
+sewsenwswswnwswswswswswesweswswswswnwsww
+nesenwsweswwwneeneswnwnwswweeesw
+eeseneseseseeeeswwwenweseseewse
+swsenwneenwnenenenwnwwnesw
+wnwwwseswnwnwnenwneenwwwnwswnwew
+nwneneneneeneeneneneeeswene
+senwnesenewsesesenweswsesenesesesesew
+eneneswwwneeewswsenesewwenenewse
+eeswwwneeswneswneenwwnenenewnwnenese
+eeeeeeeeneesw
+wwewewwwwwwewnwswwwnwnwe
+eseeeeewwswenwweseeeeneee
+seeswseseseseseseesenwsese
+nwwnwnwnwnweswnwwewnwsesenenwwenw
+seseseswneeseswwnwsenesesenww
+swnwseswseswseswswswseeswswswswnewnwsw
+swswswsweswswswswwswswne
+eeswnweswswewwesenwseeenewnenw
+swswnewweenwnwwsenenewswswswwwsewe
+seswswswswswneeswwswswnwseswseswswsenwne
+eneneenenwwsewneenwsweeesweww
+swweswseswnwswswswswseswnwseswswswnwswsw
+eswnwnwnwswnenwnwnwsenwewnwnenenwnwnw
+seseswsewsesenesesee
+wwwewwwnwwnwwwwwnesw
+esenwseseewnesewswseneewseseswnewse
+swseswnenwwseweseseswnwwwswnwnewswswne
+seneseseseneseeseswwewseneneewswnwe
+nwneeswwwnwsewwse
+sesesenenwsesenwwesesesewnesenesewese
+nwswseseseesesesesese
+nenwnwnenewnwnwnwnenwseswneneswnwnwswnesw
+seneesenewnwnwswswsenwnwnwneswnwnwnwwnww
+nenwwwwwwesenewesewwwnewsew
+nenwneeeneeneseeneeenee
+swnewnwnwewswseseswswswswswsenwwsww
+senwnwewwwnewnwswwnenewnwnwnwwnwsw
+nenenenenwneneenenenenenesw
+eeeneneneeeenwseee
+eneeenenenweweneeeeswenewsene
+sewnwwseswewneenwwnwswwsewseswsww
+enwseesenwsewnwswewneese
+wswswswwneswsewswneweswwswwswww
+swswsesenenwswsesenewseseseeseneewsese
+sewnwnwnwnwnenwenwsenwsenwnwnwwnenwwnwse
+neswnwsesenwsesewseswneswswswseswswswwse
+seswswseseswseseseneswswnwswnwseswswse
+esewnenewenewwswnesenwenenenwsese
+eeseeeswneewsewseenwwswsenesene
+enwsweeeeenwwseneweeeswnesww
+seneneeenewweeneenenewseneswnwene
+enwswnwswenwwsesenwnwwewnewswswsese
+neswnwwneeseeeenenesenwnenenenenene
+swwwewwewwsewsewwwwwnwnewnew
+eesweeeweseeweeweeneeenee
+swnenwswnesweswnwsenwneneeswenwswnenwene
+swnwwwwwwwwwwswwswneeenwwww
+nwnwnwwsewewewnwnwnwnweswnwneswswnw
+nwsewseswsenwnwneewwnw
+nenenwwsenwnenenewnenwneneneneswnwnwswse
+eswwswswnesenwsenenewsw
+seswwnewnwnwwseswswswswswsewesww
+eneswnenwnenwnwswnwnw
+weneswsenwseneweneeneenesesenewnenee
+eseenenwswnweseeseeeseeseswsesese
+swswseseseswseseswseswnw
+nwseswswwswswswswswswenewswsenwsesenenw
+nwneenenenewneneneeneneswnesewnenesw
+swswnwswnwswsenwseswswswswswswsewnwswsew
+sewseseneseswseswsewnewwswwenwnwnenese
+nwnwwnwnwenwsenwwwwnwwnwnwsenwwsew
+wswnwnwnwenenwnwneseswwenwnwenwswnew
+wnwwwwwsenwsenwwwwwnewsew
+seswseseswsesewseenesewnesenesewsesese
+enwswwswswswwnwwnwswwsewwenenwew
+neweneneeeeeneeenene
+seeseeeenweneseneweesewnwseseeese
+nesewnwenwnwneenwwswsenwne
+senwswnwenwnenwnenwnwwnwnwwewnwneenwne
+nenwnwnwneeneswnwnwnwsewnenese
+swwwsewneswswwwswesewwnewnewnww
+swswneswswswsewswswswswnwwsesweneswesww
+wswswwwwsewwswswswnw
+ewneenwwneseneseeswenenesewnewne
+seswswsenenewswseseswswwneweswsewse
+enewnwwwswenwnwewnwnwnwswesesee
+seswswseswsweswnwseswnwswswswswse
+nwseseswswseswswswneswseswswsesweswswne
+eeeweenweeeeeeeeeswseswnwe
+neneeeeeseeeeenw
+eewseeeeeeeeeee
+wwnenwnwnwwseenwwwnwwwneeswswwww
+neneswnwnewnwwsenee
+seswnwneeenwswswnwnenwwnwnwwwnwsenw
+swswswseswnwsweseswnwseswswneswswswsese
+seneseenwswseseseseseswsesesese
+nenwseswswseeswseseseseseseneseesese
+swnwnenenwneenwsenenwwneswsenwnenenenw
+neswseeswneswsesenwsewse
+sesewneneswswseseenenewsesesewnesew
+eeswseewswneeneseneswenesweew
+neswnwnwwewsweswnwenw
+swwenwseswseswseseneswsewseseseseese
+esewsewnewnwwwwneswnwswwwwnenesww
+nwsenenwnenenewnwsenwnwswnwwewswwsw
+nwseeeseeeeeneweeewneseenwee
+nenweneswswseeswneeeneeeeneneswne
+weeseewseeseneseesewseeseneeew
+nwsesenenwneneseeenewenewswwneseww
+neswnwswneswseneeeneneneneswnweenesw
+ewswswswswswswsenwne
+senwwnwnwswwnwnwnwnwnenwsenwswseenenw
+eswesenenenewneneneneeeenwne
+eeseeswnewsewseeesenwesweseeenee
+nwnewswesesewseswsewneeneneswsw
+wneneneneswneneswenenenwneneneenenene
+nwnwswneneeneewneneswewenenenenee
+swneeneeeneeesw
+nenwnenwneswnwwnenwseseseneneneneneneswne
+swseseswneswneseswswseneswswwne
+swneseswnwsenwnenwwenwenwnwnwnenwwne
+nwewwswswwewwswswseewnenwseww
+esewsesenesesesenesesewesesesew
+neneneeseswnwseswswnwsewsw
+seseeewseneseseseseeeswe
+wwsewswswnwwwwenwwwwewswww
+sewsenwnwseswwswseseseeswneseseseswswe
+seseswswseseswnwswsweswswwse
+swnewswsenenwnweseneswwnwnwnww
+wnwseneenwsesenenewnenwwnenewnenwnee
+nwnwnwnwnwenwswnwenwswenwwswnwsewnwnwe
+wwswswsweneswswswenewswwswswwswew
+nwnwnenewnenweseeneseneswneenenesenese
+swwseswenenenwswswswswswswswwswswseswsw
+senenesenenwnewnenwneswnenwnenenenenwne
+swswnweswenwnewswnwneeeswnenewswswnw
+swseswwnenweseswsesesewneeswsenwseswe
+nwsewwwswswwneswweswwswwsewww
+esesewneenenwneeneneweeenee
+swseseesesesesesesewnweneneswwnwnwse
+neseseseseswwsesesese
+ewenwwnweswseeeee
+enenwnwwewenwseseseeeneseenenwe
+neenwseenwnwnwwwnwnwnenwnenwnwnwnwswne
+wnenwnwnwneneenenwnesewnenwnenewsenenw
+nenenewsenwnenwnenwenenesenewsenenenene
+senwneseneeeswsweswnenwwenenwnene
+ewnwwnwnwnwnwnwnwnwsenwnwswnwwnw
+wwswsewswewwwwwewsenwswwne
+wnwwswwwwnenwwnwswwwwsene
+nwnewwwnwwewwwewnwswwwsewww
+eeeesweneeneswenenewnwneeeee
+nwnwnwewwenenwse
+neenewnesenenenwnenwnwnenene
+seswswswswswnewseseswneseswseneswwsesw
+newnwesewnenwnwsenenwnenwnwnwenwnwsw
+wnwwnwwnwwwneswwwswnewse
+esenweesesweeseseese
+swnwnwseswwnwseswseeseseseeseswsesesenwnw
+eeswneeswnenenewnwnenenenenesweswnwse
+wswswwneswseseseneswswneswwnewwwse
+wwneneneneeewswneeneenweswesee
+swseswswswswnwswsesesene
+swnwswswswswswswwesw
+newneneeneneneenesenenene
+eseswnwswswswswnweswseseswseswsesesesenw
+weseeseeeeeweeneeneewnenee
+ewwwwwnwwwwswwwswwweenww
+nwnwnwnwenwnwwwsewwewwwwnenww
+neeseeeseewswseneswwenwwwneswsese
+seswseseweswswsesesesw
+swnwsesweseseswwseswseswswneseswesenwse
+swseweneswnwwsweswenewseswsewnwnwne
+nwnwswswnwwnwenwswsenwnwnesene
+swnenwswswswswswneseswswwseswswseswswsw
+nenwnwneseneswnenwnenene
+nenewseswsenesesesesesesesesesesesewwse
+senewswnenenenesew
+eneswwseewseseeseenwewnenenwesese
+nwsenenenewneneseeneenenwnenenenenesw
+weswsenwswnwsesenesenwweneseeswsewsw
+eeseseseseseswsesesenese
+swwwnwwnwenwwnewswnwnewwnenwsesw
+weewneeeeeewsweeseeeeseee
+sewnwseseseeneswsewseseweseseswsese
+swnenwseenenwsewsenenewwnenenenwenene
+wwswwnwwseswneneewwwswnewwneseww
+senewseseseseseseenwsewseneswsesesesenw
+seneneneweneenenenenwneneneneswnenew
+eesweeneeeeeewswneeeeee
+wwwswnewwwwew
+nweeseseneeenwnweseweswsweee
+nesewswswneseewnenenweneeswnesweene
+nwwsewewwwweewewwswswwww
+wswwnwwnewwwenenwwseswnwewwwse
+wnenenwneeswnenwnwnwnenenwswnwnweswenw
+eseswswwnwswswnesenwseswswseenwnwsenw
+nenwnwnwneneswnwnenesesenwnenwnwwnwnwne
+eswnenwswnwswneenweneswnenenw
+nwnwnewwnwnwweswneswnwwnwnwnwwsewnw
+wswnwswswewswwswwsww
+swwnwwswswswweswswsweenwswwwswsww
+wwwwewwwwwwwwwnw
+senesenenenwnwnwnwsenenw
+nwwnwwsenwwnwnwwenwnenww
+nwnewnenwnenesenesenesenwnew
+nwnwsenwswnwwwsenwenwenwnwnwnwsenwse
+eswswsesewswnenesenwenwesesesenwswsw
+nenewnenewswwswswsesenenenewnwnwsenwsw
+nwsenwnewnenwnwnwswwswnesenenewenesenwse
+swseneseseseewnweseswswswswewswnwnese
+nwnenwnwseseeesesenwnwswsesewnweseeesw
+wnwnwwnwnwnesenwnwnwseneseswnwnwnwnwnenwe
+neewweswseneesesewnenewsesesenese
+eewesweeeeswewneeneneneneene
+neneeneeswneeneeneneenwnee
+nwnwwsewsenwwnenwsewwnwnwnenwenwwnwnw
+sesewswseswswseswesweswswswswswnwnwsw
+swnwneenwnenewenwnwnwewseenwswesw
+swnwenwnwneenenwnenwnwenwswnenenwswne
+wsesweneseswswwwswswswnwsenwneenweww
+wsenweeeeseeeeeesesewewesew
+neswnwnwnwnwswesesenwneswnw
+swwswswswneswsweswwswswneneswseeswswsw
+sweswenwnesweeenweswnwsesenewwenw
+senenwnwnewnwnenenenwnenw
+nenwneneswnwnenenwnwe
+nenenenwsewesweenewneneseweneee
+eseswsenewnewsesewswwnwenesesesesese
+neneenenenwneswnene
+neswnwnwnenenenwnwswnenwnenwwe
+eeswswnwsweeeeneene
+eswswswnwnweswswswnwswswseswswswwswe
+newswnwnwsweeneneeswnwseswnwenwnwnenwnw
+nwwnwsewneswwwwnwsewsweewswneswsew
+newsewnenenenenesweneeewwnwnesenew
+wnewenwsewwwwnenwnwswnwwwwwsenw
+neneeenweeeswneseenesenwsewesenw
+neswswswswswswswseseenw
+enwnwnenwswwnwneenwnwswnwnenenwnwnwenw
+newswwsenewwwwwwseeswsewwwwswne
+wwseseseseseseseenesenwneseseeseswsese
+wswnwnwwneswsewwnenwswnwenwnwwnww
+sesesewesewneswneneneseewsweeseswe
+eneeswswneneseneneneenewnew
+eeswnenenenwnwwnenwnenenwnenwnesenwne
+seswseswnwneswswsesenwesesesenwesewseesw
+seneeswwnwneswwswnwwwwewwnwnwe
+wwweswwwsewwwnwwwww
+seeneneeweneeeenwee
+swnwnenwnwnwnwnenwwnwnwnenwsesw
+nwwwenwwnwswwwnwnwsenw
+eweeesenewneneneneeenene
+nwwswnwnesenwnwnwnwnwnwswnenenwnwnwnwnw
+eswseeseseswnwsesenwsenwseseswswseswswsw
+enwseeeenwnwneseeswswweeeswese
+wneeneeneneneneenenenese
+neesesesesewenewsew
+nenwsenwenwnesewnesenewwenwnwnwsene
+neeeseesweneeesenwseswesweeew
+seswswwsewnwswswwneswswswswseswwnwsw
+sewwseseseneeseseesesesesewsesesese
+swwneseswswswswseswswswseneweneseswswsw
+swswwswswsweneswnwnwseseseswswswseswsw
+swenwnenwnwnenenwnenenenenwne
+seeseseeseeesesesenesewswsese
+wwswwwwwnwwwwewwswnwwneesese
+wneneenwneenenenwnwneswnenenwnwnenwnwsesw
+wwnewswwswsewwneswwnewsewwww
+wswseswswnwseswewwswwsenewswswnenw
+seneweenenwneeeeeeeeesw
+esesewnwnwnwwwswnenwwweneswswne
+senwwwswwswwwweewwwswnenwsww
+ewswswseneeeenwneseswseneesewnwew
+nenwnwnwneseswenwseesenenenwwnwnwwnwnw
+seseesesenweneseseneeeeeseswsewe
+sewweneswneswswwswswwswwnwswswswwsw
+wseneweweswsenenenesewweswnwsewsese
+nenwnwswnweesenwnwswnwnwwwewweswnwse
+senwseseseseseswseseswsesenesese
+"""
 
+enum TileColor {
+    case black
+    case white
+}
+
+enum Direction: String, CaseIterable {
+    case e
+    case se
+    case sw
+    case w
+    case nw
+    case ne
+}
+
+struct Point: Equatable, Hashable {
+    let x: Double
+    let y: Double
+
+    init(x: Double, y: Double) {
+        self.x = (10_000 * x).rounded() / 10_000
+        self.y = (10_000 * y).rounded() / 10_000
+    }
+
+    func move(in direction: Direction) -> Point {
+        switch direction {
+        case .e: return Point(x: x + 1, y: y)
+        case .se: return Point(x: x + cos(Double.pi / 3), y: y + sin(Double.pi / 3))
+        case .sw: return Point(x: x - cos(Double.pi / 3), y: y + sin(Double.pi / 3))
+        case .w: return Point(x: x - 1, y: y)
+        case .nw: return Point(x: x - cos(Double.pi / 3), y: y - sin(Double.pi / 3))
+        case .ne: return Point(x: x + cos(Double.pi / 3), y: y - sin(Double.pi / 3))
+        }
+    }
+
+    var neighbors: [Point] { Direction.allCases.map(move(in:)) }
+}
+
+struct Tile: Equatable {
+    var currentColor = TileColor.white
+    var point = Point(x: 0, y: 0)
+
+    mutating func flip() {
+        switch currentColor {
+        case .black:
+            currentColor = .white
+        case .white:
+            currentColor = .black
+        }
+    }
+
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.point == rhs.point
+    }
+}
+
+func parseDirections(_ input: String) -> [Direction] {
+    var result = [Direction]()
+    var currentDirection = ""
+
+    input.forEach { character in
+        currentDirection.append(character)
+
+        if let direction = Direction(rawValue: currentDirection) {
+            currentDirection = ""
+            result.append(direction)
+        }
+    }
+
+    return result
+}
+
+func movePoint(_ directions: [Direction]) -> Point {
+    var point = Point(x: 0, y: 0)
+    directions.forEach { point = point.move(in: $0) }
+    return point
+}
+
+func finishRenovation(_ input: String) -> [Tile] {
+    var tiles = [Tile]()
+
+    input.components(separatedBy: .newlines).forEach { line in
+        let point = movePoint(parseDirections(line))
+
+        if let existingIndex = tiles.firstIndex(where: { $0.point == point }) {
+            tiles[existingIndex].flip()
+        } else {
+            tiles.append(Tile(currentColor: .black, point: point))
+        }
+    }
+
+    return tiles.filter { $0.currentColor == .black }
+}
+
+//print(finishRenovation(input).count)
+
+// run in Xcode project!!
+func blackTiles(_ input: String, after days: Int = 100) -> Int {
+    var blackPoints = finishRenovation(input).map(\.point)
+
+    (1...days).forEach { _ in
+        var allPossiblePoints = blackPoints
+            .map(\.neighbors)
+            .reduce(into: Set<Point>()) { result, array in
+                array.forEach { point in
+                    result.insert(point)
+                }
+            }
+        blackPoints.forEach { allPossiblePoints.insert($0) }
+
+        blackPoints = allPossiblePoints.filter { point in
+            let blackNeighbors = point.neighbors.filter(blackPoints.contains)
+            if blackPoints.contains(point) {
+                return !(blackNeighbors.count == 0 || blackNeighbors.count > 2)
+            } else {
+                return blackNeighbors.count == 2
+            }
+        }
+    }
+
+    return blackPoints.count
+}
+
+print(blackTiles(input))
